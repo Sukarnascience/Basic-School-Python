da515f93ca8dd2599c4220927a42046e469bf242
sukarna jana <47217084+Sukarnascience@users.noreply.github.com>
2020-11-04 08:43:35 +0530

    Create PositiveNegativeZero.py
    Write a program to check the given number is +ve, -ve, 0.

---
+++ PositiveNegativeZero.py
@@ -0,0 +1,9 @@
+# Write a program to check the given number is +ve, -ve, 0.
+
+x = float(input("Enter Value"))
+if(x<0):
+    print("It's Negative")
+elif(x==0):
+    print("It's Zero")
+else:
+    print("It's Positive")
