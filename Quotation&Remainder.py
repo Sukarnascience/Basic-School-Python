476fe6f5be6cc53d411cf5d1ccf104ca835073b9
sukarna jana <47217084+Sukarnascience@users.noreply.github.com>
2020-11-04 08:22:06 +0530

    Create Quotation&Remainder.py
    Write a program to find Quotation and Remainder of 2 numbers read by the user

---
+++ Quotation&Remainder.py
@@ -0,0 +1,7 @@
+# Write a program to find Quotation and Remainder of 2 numbers read by the user 
+
+a = float(input("Enter First Value:-"))
+b = float(input("Want to devide by:-"))
+c = a/b
+d = a%b
+print("Remainder =",d," Quotation =",c)
