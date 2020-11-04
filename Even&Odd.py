ee4411386b20b4f011a59c5eb8b291ba75436f74
sukarna jana <47217084+Sukarnascience@users.noreply.github.com>
2020-11-04 08:26:15 +0530

    Create Even&Odd.py
    Write a program to check a given number is even or odd

---
+++ Even&Odd.py
@@ -0,0 +1,7 @@
+# Write a program to check a given number is even or odd
+
+x = float(input("Enter Value:-"))
+if(x%2==0):
+    print("this ",x," is even number")
+else:
+    print("this ",x," is odd number")
