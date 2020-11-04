840ce271490f0d876e729cddb1c6018f7139003a
sukarna jana <47217084+Sukarnascience@users.noreply.github.com>
2020-11-04 08:14:34 +0530

    Create FindAverageNumber.py
    Write a Program to find the average number of 5 subject

---
+++ FindAverageNumber.py
@@ -0,0 +1,9 @@
+# Write a Program to find average number of 5 subject
+
+a = float(input("first subject marks:-"))
+b = float(input("second subject marks:-"))
+c = float(input("third subject marks:-"))
+d = float(input("forth subject marks:-"))
+e = float(input("fifth subject marks:-"))
+f = (a+b+c+d+e)/5
+print("the average of 5 Subject is ",f)
