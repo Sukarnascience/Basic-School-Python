d4f209a5af2297a5e7af209b2996b4914dd34dbb
sukarna jana <47217084+Sukarnascience@users.noreply.github.com>
2020-11-04 09:41:47 +0530

    Create Eligible2Vote.py
    Write a program to check the given person is eligible to vote or not

---
+++ Eligible2Vote.py
@@ -0,0 +1,7 @@
+# Write a program to check the given person is eligible to vote or not
+
+a = int(input("Age of a persion: "))
+if(a>=18):
+    print("Yes,The persion is eligible to vote")
+else:
+    print("No,The persion is not eligible to vote")
